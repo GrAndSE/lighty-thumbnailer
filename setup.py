d35c5464ba5a6b720b445804ce665c6d409176eb
Andrey Grygoryev <undeadgrandse@gmail.com>
2011-11-14 16:12:13 +0200

    Add a setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+        name='lighty-thumbnailer',
+        version='0.1',
+        description='sorl.thumbnail inspired image thumbnailer',
+        long_description=open('README').read(),
+        author='Andrey Grygoryev',
+        author_email='undeadgrandse@gmail.com',
+        license='BSD',
+        url='https://github.com/GrAndSE/lighty-thumbnailer',
+        packages=find_packages(),
+        platforms="any",
+        zip_safe=False,
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Multimedia :: Graphics',
+        ],
+)
